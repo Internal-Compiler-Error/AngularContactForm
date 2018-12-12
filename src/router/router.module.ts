32a602dd45e12a24b711571753c5e8fd267c774a
Bill Wang <bill79231725@gmail.com>
2018-12-12 13:30:47 -0500

    remove unneeded import and declaration

--- src/router/router.module.ts
+++ src/router/router.module.ts
@@ -1,25 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {RoutingComponent} from './routing/routing.component';
 import {WelcomeComponent} from '../app/welcome/welcome/welcome.component';
-import {ContactFormListComponent} from '../contact-form/contact-form-list/contact-form-list.component';
-import {ContactFormComponent} from '../contact-form/contact-form/contact-form.component';
 import {WelcomeModule} from '../app/welcome/welcome.module';
 import {ContactFormModule} from '../contact-form/contact-form.module';
 
 
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'contact-form-list', component: ContactFormListComponent},
-  {path: 'contact-form', component: ContactFormComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'welcome'}
+  {path: '', pathMatch: 'full', redirectTo: 'welcome'},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-  declarations: [
-    RoutingComponent
-  ],
+  declarations: [],
   imports: [
     WelcomeModule,
     CommonModule,
